bde64d8d76bb615c9260d21d4833f8e165d14f93
Sid1298 <57630798+Sid1298@users.noreply.github.com>
2019-11-11 21:46:25 +0530

    Update Tic_Tac_Toe_Game.py

--- Tic_Tac_Toe_Game.py
+++ Tic_Tac_Toe_Game.py
@@ -1,11 +1,38 @@
+'''
+This is a program for a simple Tic-Tac-Toe game.
+The board layout is as follows:
+
+   |   |   
+ 7 | 8 | 9 
+___|___|___
+   |   |
+ 4 | 5 | 6
+___|___|___
+   |   |   
+ 1 | 2 | 3
+   |   |
+   
+The actual board will be of list type having index from 0-9.
+(note: here the 0 index is reserved as '#' so index 1 - 9 are available for positons on the board).
+The numbers in the board above correspond to the number positions on the numpad of a keyboard.
+These numbers are used as a reference to place marks by players on their turn to these positions on the board.
+'''
+
 import random
 
 
 def display_board(board):
-    print('\n' * 100)
-    '''print(board[7] + '|' + board[8] + '|' + board[9])
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print(board[1] + '|' + board[2] + '|' + board[3])'''
+       
+    '''
+    Takes input : a board of list type
+    Output : prints out the updated board
+    This function will be used to display the game board.
+    After every move by the players, the board will be re-printed.
+    To clear the screen before the new/updated board is printed, 100 blank lines (\n) are printed.
+    '''
+    
+    print('\n' * 100) # Printing blank lines to clear screen
+    
     print('   |   |   ')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('___|___|___')
@@ -18,26 +45,48 @@ def display_board(board):
 
 
 def player_input():
-    choice = ''
-    while choice != 'X' and choice != 'O':
-        choice = input('Enter marker for Player1: ').upper()
+    
+    '''
+    Takes no input argument
+    Output : returns a tuple of character markers.
+    Function to ask players if they want the (X) or (O) marker.
+    The input marker is assigned to player1.
+    '''
+    
+    choice = str()
+    while choice != 'X' and choice != 'O': # while loop for getting valid inputs from players
+        choice = input('Enter marker for Player1: ').upper() # use of .upper() to avoid the markers in lower case
 
+    # assigning markers to each player
     player1 = choice
     if player1 == 'X':
         player2 = 'O'
     else:
         player2 = 'X'
 
-    return player1, player2
+    return player1, player2 # default return type in this syntax is tuple, iterable which will be used later to access player markers
 
 
 def place_marker(board, marker, position):
+    
+    '''
+    Takes input : a board of list type, a character marker of a player, position to place the marker of integer type for index in list
+    Output : a board of list type
+    Function to place the marks by players on the board.
+    '''
+    
     board[position] = marker
     return board
 
 
 def win_check(board, mark):
-    # m = mark == 'X'
+    
+    '''
+    Takes input : a board of list type, a character marker of a player
+    Output : boolean value, true if a player wins, false if no one wins
+    Function to check for winning conditions and deciding which player wins
+    '''
+    
     # check for horizontal win conditions
     if board[7] == board[8] == board[9] == mark:
         print(f'{mark} wins')
@@ -48,6 +97,7 @@ def win_check(board, mark):
     elif board[1] == board[2] == board[3] == mark:
         print(f'{mark} wins')
         return True
+    
     # check for vertical win conditions
     elif board[7] == board[4] == board[1] == mark:
         print(f'{mark} wins')
@@ -58,6 +108,7 @@ def win_check(board, mark):
     elif board[9] == board[6] == board[3] == mark:
         print(f'{mark} wins')
         return True
+    
     # check for diagonals
     elif board[7] == board[5] == board[3] == mark:
         print(f'{mark} wins')
@@ -65,16 +116,31 @@ def win_check(board, mark):
     elif board[1] == board[5] == board[9] == mark:
         print(f'{mark} wins')
         return True
-    # no condition satisfies
+    
+    # no condition satisfies / draw
     else:
         return False
 
 
 def choose_first():
+    
+    '''
+    Takes no input argument
+    Output : a random value either 1 or 2
+    Function to decide which player gets to play first
+    '''
+    
     return random.randint(1, 2)
 
 
 def space_check(board, position):
+    
+    '''
+    Takes input : a board of list type, integer value for index position
+    Output : boolean value, true if position on the board is empty, false otherwise
+    Function to check if the position is empty/available to place a marker for the player's turn
+    '''
+    
     if board[position] == 'X' or board[position] == 'O':
         return False
     else:
@@ -82,6 +148,13 @@ def space_check(board, position):
 
 
 def full_board_check(board):
+    
+    '''
+    Takes input : a board of list type
+    Output : boolean value, true if board is full, false otherwise
+    Function to check if the board is completely full and no place is available to place a marker
+    '''
+    
     for mark in board:
         if mark == ' ':
             return False
@@ -90,6 +163,13 @@ def full_board_check(board):
 
 
 def player_choice(board):
+    
+    '''
+    Takes input : a board of list type
+    Output : returns position to place the marker on the board
+    Function to take in a value for position to place marker on the board and return that position index if space is available
+    '''
+    
     choice = eval(input('Enter position to put your mark : '))
     if space_check(board, choice):
         return choice
@@ -98,6 +178,13 @@ def player_choice(board):
 
 
 def replay():
+    
+    '''
+    Takes no input
+    Output calls function to replay the game, returns false if no
+    Function to ask the players for a rematch/new game
+    '''
+    
     rep = input('Do you want to play again? \nYes / No: ')
     if rep.lower() == 'yes' or rep.lower() == 'y':
         play_game()
@@ -142,4 +229,6 @@ def play_game():
         print('Thank you for playing')
 
 
-play_game()
+# Now to play the game
+if __name__ == "__main__":
+    play_game()
